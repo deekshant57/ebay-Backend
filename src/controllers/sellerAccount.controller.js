3caca05ae6fafe371a44975d66c01fda8e367c37
deekshant57 <deekshant57@gmail.com>
2022-03-29 17:45:52 +0530

    Updated Auth.js

--- src/controllers/sellerAccount.controller.js
+++ src/controllers/sellerAccount.controller.js
@@ -1,1 +1,16 @@
 const Seller = require("../models/sellerAccount.model");
+
+const express = require("express");
+const authenticate = require("../middlewares/authenticate");
+
+const router = express.Router();
+
+// Register a user
+router.post("", authenticate, async (req, res) => {
+  try {
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+});
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const { register, login } = require("./controllers/auth.controller");
 
 const app = express();
 
 app.use(express.json());
+
+app.use("register", register);
+
+app.use("login", login);
 
 module.exports = app;
