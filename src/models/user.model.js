bab8e1099d684ff8436eebbc8658780e4683a160
arpankushwah8 <96377007+arpankushwah8@users.noreply.github.com>
2022-03-30 07:12:10 +0530

    password
    password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,6 +4,7 @@
 // password :
 
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
 const userSchema = new mongoose.Schema({
     firstName: {type:String, required:true},
     LasttName: {type:String, required:true},
@@ -11,5 +12,19 @@ const userSchema = new mongoose.Schema({
     password: {type:String, required:true},
     
 
-},)
+},
+{
+    timestamps : true,
+    versionKey : false,
+})
+userSchema.pre("save", function(next){
+    const hash = bcrypt.hashSync(this.password, 8);
+    this.password = hash;
+    return next();
+})
+
+userSchema.methods.checkPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
 module.exports = mongoose.model("user", userSchema);
+module.exports = User;
