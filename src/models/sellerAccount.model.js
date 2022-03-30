204dafdbf9ba2c5833e1a98ef3003dbfc08471c4
ayshasinara <97454953+ayshasinara@users.noreply.github.com>
2022-03-30 12:28:08 +0530

    product and seller model

--- src/models/sellerAccount.model.js
+++ src/models/sellerAccount.model.js
@@ -1,1 +1,25 @@
 // sellerName, sellerEmail , password, sellerLocation;
+const mongoose= require("mongoose");
+const bcrypt = require("bcrypt")
+const sellerSchema = new mongoose.Schema({
+    sellerName:{type:String,required:true},
+    sellerEmail: {type : String, required : true, unique:true},
+    password : {type : String, required : true},
+    sellerLocation:{type:String ,required:true}
+},{
+    timestamps : true,
+    versionKey : false,
+})
+sellerSchema.pre("save", function(next){
+    const hash = bcrypt.hashSync(this.password, 8);
+    this.password = hash;
+    return next();
+})
+
+sellerSchema.methods.checkPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
+
+const Seller= mongoose.model("seller", sellerSchema)
+
+module.exports = Seller;
