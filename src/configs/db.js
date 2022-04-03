e11bb34b0b27b6cf17cdf5776436f53d2aa56d77
dipakdushing <97525751+dipakdushing@users.noreply.github.com>
2022-04-03 22:09:50 +0530

    Update db.js
    update

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  return mongoose.connect("mongodb://127.0.0.1:27017/");
+  return mongoose.connect("mongodb+srv://deekshant57:deekshant_123@practice.9w926.mongodb.net/myFirstDatabase?authSource=admin&replicaSet=atlas-mk2ptn-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true");
 };
